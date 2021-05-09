dac37bb1d113ec884a456ef09688a9801a5fdc64
array <argha@hey.com>
2021-05-10 00:54:59 +0530

    some more changes

--- API/Startup.cs
+++ API/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
+using Core.Interfaces;
+
 namespace API
 {
     public class Startup
@@ -18,7 +20,7 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddScoped<IProductRepository,ProductRepository>();
             services.AddControllers();
 
             services.AddDbContext<StoreContext>(options =>
